31aa2319e921872cbab6689ad312bd0f58a9ee6e
jaolsen7 <jaolsen@berkeley.edu>
2022-01-12 14:33:36 -0800

    fixing link

--- assets/js/scriptGecko.js
+++ assets/js/scriptGecko.js
@@ -1,4 +1,4 @@
-var requestUrl = "https://api.coingecko.com/api/v3/simple/price?ids=" + search + "&vs_currencies=USD&include_last_updated_at=true";
+var requestUrl = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=USD&include_last_updated_at=true";
 
 fetch(requestUrl)
         .then(function (response) {
