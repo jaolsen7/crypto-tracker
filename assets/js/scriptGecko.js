bdb7fa6ad84411676212a28ad2500f019b3969a4
jaolsen7 <jaolsen@berkeley.edu>
2022-01-13 14:17:26 -0800

    gecko integration

--- assets/js/scriptGecko.js
+++ assets/js/scriptGecko.js
@@ -1,25 +1,40 @@
 // var favorites = [];
 // var
 
-// $("#search-bar").click(function (event) {
-//   var searchInput = $("input").val().trim();
-//   event.preventDefault();
-//   getGecko(searchInput);
+$("#search").click(function (event) {
+  var searchInput = $("input").val().trim();
+  event.preventDefault();
 
-//   function getGecko(searchInput) {
-//     var requestUrl =
-//       "https://api.coingecko.com/api/v3/simple/price?ids=" +
-//       searchInput +
-//       "&vs_currencies=USD&include_last_updated_at=true";
+  getGecko(searchInput);
+});
 
-//     fetch(requestUrl)
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (data) {
-//         console.log(data);
-//         console.log(data.bitcoin.usd);
-//         //console.log(data.searchInput.usd);
-//       });
-//   }
-// });
+var geckoEl = document.querySelector("#api-container");
+
+function getGecko(searchInput) {
+  var requestUrl =
+    "https://api.coingecko.com/api/v3/simple/price?ids=" +
+    searchInput +
+    "&vs_currencies=USD&include_last_updated_at=true";
+console.log(requestUrl);
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        var price = data[searchInput].usd;
+        var unix = data[searchInput].last_updated_at;
+
+        var date = new Date(unix * 1000);
+        var dateObject = "Date: " + date.toLocaleString().split(",")[0];
+        var dateLi = document.createElement("li");
+        dateLi.append(dateObject);
+        geckoEl.append(dateLi);
+
+        var priceLi = document.createElement("li");
+        var priceObject = "Price: " + price + " USD";
+        priceLi.append(priceObject);
+        //priceLi.setAttribute("class", "list-group-item");
+        geckoEl.append(priceLi);
+    });
+};
