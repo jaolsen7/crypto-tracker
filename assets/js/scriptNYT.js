d8b1c7c936ab98943e940d8a4d464900c6d9e4d4
TylerOliveira <55139667+TylerOliveira@users.noreply.github.com>
2022-01-13 14:43:59 -0800

    news headline and link function

--- assets/js/scriptNYT.js
+++ assets/js/scriptNYT.js
@@ -1,11 +1,37 @@
-var requestUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=bitcoin&api-key=pwSjTOg5rhnaPpEuYF8qONwQlPcvBJbR";
-
-fetch(requestUrl)
+var resultContentEl = document.querySelector('#api-container');
+function getNYT(search) {    
+        var requestUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search +"&api-key=pwSjTOg5rhnaPpEuYF8qONwQlPcvBJbR";
+        fetch(requestUrl)
         .then(function (response) {
-            return response.json();
+                return response.json();
         })
         .then(function (data) {
-                //console.log(data)
-                console.log(data.response.docs[0].headline.main)
-                console.log(data.response.docs[0].web_url)
+                console.log(data)
+                //console.log(data.response.docs[0].headline.main)
+                //console.log(data.response.docs[0].web_url)
+                var resultCard = document.createElement('div');
+                resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+              
+                var resultBody = document.createElement('div');
+                resultBody.classList.add('card-body');
+                resultCard.append(resultBody);
+              
+                var titleEl = document.createElement('h3');
+                titleEl.textContent = "News:  " + data.response.docs[0].headline.main;
+              
+                var bodyContentEl = document.createElement('a');
+                bodyContentEl.textContent = "link";
+                bodyContentEl.setAttribute("href", data.response.docs[0].web_url);
+                //<link href="/media/examples/link-element-example.css" rel="stylesheet"></link>
+                // "<link href=" + data.response.docs[0].web_url +">" + '<br/>';
+
+                  resultBody.append(titleEl, bodyContentEl);
+//resultCard.append(resultBody);
+                  resultContentEl.append(resultCard);
         });
+}
+$("#search").click(function (event) {
+                var searchInput = $("input").val();
+                event.preventDefault();
+                getNYT(searchInput);
+                });
