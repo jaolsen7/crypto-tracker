7fa83bf22aacc6898f05e67201cfe85ebf3b4684
jaolsen7 <jaolsen@berkeley.edu>
2022-01-13 10:39:39 -0800

    console log usd and commented function

--- assets/js/scriptGecko.js
+++ assets/js/scriptGecko.js
@@ -1,3 +1,6 @@
+//var search = input;
+//function getGecko(search) {
+
 var requestUrl = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=USD&include_last_updated_at=true";
 
 fetch(requestUrl)
@@ -5,5 +8,8 @@ fetch(requestUrl)
             return response.json();
         })
         .then(function (data) {
-                console.log(data)
-        });
+                console.log(data);
+                console.log(data.bitcoin.usd);
+                //console.log(data.search.usd)
+        });  
+//}
